50ead4a13f9940d2354d89cf46c0b60e240ab411
Becca Rothwell <becca.rothwell@gmail.com>
2023-11-03 12:41:00 +0000

    feat: Add switch statement to convertNumberToNumeral for each single numeral character value

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export function convertNumberToNumeral(num: number) {}
+export function convertNumberToNumeral(num: number) {
+  switch (num) {
+    case 1:
+      return "I";
+    case 5:
+      return "V";
+    case 10:
+      return "X";
+    case 50:
+      return "L";
+    case 100:
+      return "C";
+    case 500:
+      return "D";
+    case 1000:
+      return "M";
+    default:
+      return "Cannot parse this number to numeral yet";
+  }
+}
